9be258bafb34c59153c6be85f2719195cd684645
joel rojas maldonado <sistemasUAP@outlook.com>
2021-01-26 16:46:42 -0500

    Subida Roles

--- FUENTE/SysControlWebMG/Entidades/RolCLS.cs
+++ FUENTE/SysControlWebMG/Entidades/RolCLS.cs
@@ -6,6 +6,7 @@ namespace Entidades
     {
         public int IdRol { get; set; }
         public string NombreRol { get; set; }
+        public string UsuarioCreacion { get; set; }
         public DateTime FechaCreacion { get; set; }
         public string UsuarioModificacion { get; set; }
         public DateTime FechaModificacion { get; set; }
